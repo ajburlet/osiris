a544e2cbd3d88bc6c3b251cb8c893f244f2309db
Alain-Jacques de Burlet <ajburlet@hotmail.com>
2016-04-07 23:16:47 -0300

    Including shaders, that were missing in the last commit

---
+++ Osiris/OsirisSDK/shaders/Vertex-OutputSmoothColor.glsl
@@ -0,0 +1,16 @@
+#version 330
+
+layout (location = 0) in vec4 position;
+layout (location = 1) in vec4 color;
+
+smooth out vec4 smoothColor;
+
+uniform mat4 perspectiveMtx;
+uniform mat4 modelMtx;
+uniform mat4 cameraMtx;
+
+void main()
+{
+	gl_Position = ((position * modelMtx) * worldMtx) * perspectiveMtx;
+}
+
