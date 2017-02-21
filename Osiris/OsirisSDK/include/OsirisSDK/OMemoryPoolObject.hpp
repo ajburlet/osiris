9f63c2f1da891be2d9781c3cccc6ebead392157c
Alain-Jacques de Burlet <ajburlet@hotmail.com>
2017-02-21 17:56:44 -0300

    OMemoryPoolObject documentation.

--- Osiris/OsirisSDK/include/OsirisSDK/OMemoryPoolObject.hpp
+++ Osiris/OsirisSDK/include/OsirisSDK/OMemoryPoolObject.hpp
@@ -3,16 +3,37 @@
 #include "defs.h"
 #include "OMemoryPool.h"
 
+
+/**
+ \brief Template class for objects that use memory pools.
+ 
+ Classes of this type can take advantage of memory pools to avoid frequent malloc() and free() calls,
+ avoiding heap fragmentation. Inheriting this template class will overload new and delete operators.
+
+ \tparam blockSize Block size in bytes.
+ \tparam segmentSize Number of blocks in a segment.
+
+ */
 template <size_t blockSize, size_t segmentSize>
 class OAPI OMemoryPoolObject 
 {
 private:
 	static OMemoryPool* _memoryPool;
 
+	/**
+	 \brief Initializes the memory pool. Used by the new operator.
+	 */
 	static void Init();
 
 public:
+	/**
+	 \brief Class constructor.
+	 */
 	OMemoryPool();
+
+	/**
+	 \brief Class destructor.
+	 */
 	virtual ~OMemoryPool();
 
 	void* operator new(size_t sz);
@@ -30,7 +51,6 @@ inline void OMemoryPoolObject<blockSize, segmentSize>::Init()
 template<size_t blockSize, size_t segmentSize>
 inline OMemoryPoolObject<blockSize, segmentSize>::OMemoryPool()
 {
-	Init();
 }
 
 template<size_t blockSize, size_t segmentSize>
