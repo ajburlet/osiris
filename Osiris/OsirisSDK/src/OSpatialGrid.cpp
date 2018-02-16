780e8bd850e9f2c40bf3e6406a820c3db970016c
ajburlet <ajburlet@gmail.com>
2018-02-16 17:16:12 -0200

    Adding spatial grid class designed to deal with collisions -- skeleton only.

---
+++ Osiris/OsirisSDK/src/OSpatialGrid.cpp
@@ -0,0 +1,108 @@
+#include "OsirisSDK/OBaseEntity.h"
+#include "OsirisSDK/OCollection.hpp"
+#include "OsirisSDK/OMath.h"
+
+#include "OsirisSDK/OSpatialGrid.h"
+
+using namespace std;
+
+// ****************************************************************************
+// OSpatialGrid
+// ****************************************************************************
+OSpatialGrid::OSpatialGrid(int xCells, int yCells, int zCells) :
+	_xCells(xCells),
+	_yCells(yCells),
+	_zCells(zCells)
+{
+	_grid.resize(xCells*yCells*zCells);
+}
+
+
+OSpatialGrid::~OSpatialGrid()
+{
+	for (map<OBaseEntity*, Node*>::iterator it = _nodeMap.begin(); it != _nodeMap.end(); it++) 
+		delete it->second;
+}
+
+OCollection<OBaseEntity>* OSpatialGrid::entities()
+{
+	return &_entities;
+}
+
+void OSpatialGrid::process()
+{
+	OVector3 min, max;
+
+	/*
+	for (map<OBaseEntity*, Node*>::iterator it = _nodeMap.begin(); it != _nodeMap.end(); it++) {
+		if (it == _nodeMap.begin()) {
+		}
+	}
+	*/
+}
+
+
+// ****************************************************************************
+// OSpatialGrid::Node
+// ****************************************************************************
+OSpatialGrid::Node::Node(OBaseEntity * entity, Node * next) : 
+	_ent(entity), 
+	_next(next)
+{
+}
+
+OSpatialGrid::Node::~Node()
+{
+}
+
+OSpatialGrid::Node * OSpatialGrid::Node::next()
+{
+	return _next;
+}
+
+void OSpatialGrid::Node::setNext(Node * next)
+{
+	_next = next;
+}
+
+OBaseEntity * OSpatialGrid::Node::entity()
+{
+	return _ent;
+}
+
+// ****************************************************************************
+// OSpatialGrid::Cell
+// ****************************************************************************
+OSpatialGrid::Cell::Cell() :
+	_first(NULL),
+	_last(NULL)
+{
+}
+
+OSpatialGrid::Cell::~Cell()
+{
+}
+
+OSpatialGrid::Node * OSpatialGrid::Cell::first()
+{
+	return _first;
+}
+
+OSpatialGrid::Node * OSpatialGrid::Cell::last()
+{
+	return _last;
+}
+
+void OSpatialGrid::Cell::add(Node * node)
+{
+	if (_first == NULL) _first = node;
+	_last->setNext(node);
+	node->setNext(NULL);
+}
+
+void OSpatialGrid::Cell::reset()
+{
+	_first = NULL;
+	_last = NULL;
+}
+
