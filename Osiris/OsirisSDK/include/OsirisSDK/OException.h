028a66ec635573b4bd1e2f933e076cb3ccabcb61
Alain-Jacques de Burlet <ajburlet@gmail.com>
2020-03-31 00:27:51 +0100

    Improved exception class, more handy macros to handle exceptions and forward them.

--- Osiris/OsirisSDK/include/OsirisSDK/OException.h
+++ Osiris/OsirisSDK/include/OsirisSDK/OException.h
@@ -9,29 +9,30 @@
 #endif
 
 /**
- \brief Osiris exception class.
+ @brief Osiris exception class.
 */
 class OAPI OException
 {
 public:
 	/**
-	 \brief Class constructor.
-	 \param file Source file name.
-	 \param line Source file line number.
-	 \param what Exception message.
+	 @brief Class constructor.
+	 @param aFle Source file name.
+	 @param aLine Source file line number.
+	 @param aWhat Exception message.
 	*/
-	OException(const char* file, int line, const char* what);
+	OException(const char* aFile, int aLine, const char* aWhat);
 	
 	/**
-	 \brief Class destructor.
+	 @brief Class destructor.
 	*/
 	virtual ~OException();
 
 	/**
-	 \brief Returns the exception message.
-	 \return The exception message along with source file name and line number.
+	 @brief Returns the exception message.
+	 @return The exception message along with source file name and line number.
 	*/
 	const char* what();
+
 private:
 	std::string _file;
 	int _line;
@@ -40,10 +41,33 @@ private:
 };
 
 /**
- \brief Simplified exception constructor macro.
+ @brief Simplified exception constructor macro.
 
  Using this macro dispeses the need to enter source file name and line number.
 
- \param what Exception message.
+ @param what Exception message.
 */
 #define OException(what) OException(__FILE__, __LINE__, what)
+
+/**
+ @brief Checks if pointer is null, if it is throws an exception.
+ @param ptr The pointer to be checked.
+ */
+#define OExceptionPointerCheck(ptr) if ((ptr) == nullptr) throw OException("Null pointer exception.")
+
+/**
+ @brief Checks if pointer is null, if it is calls a callback function and throws an exception.
+ @param ptr The pointer to be checked.
+ @param cb The callback function to be called prior to the exception.
+ */
+#define OExceptionPointerCheckCb(ptr, cb) if ((ptr) == nullptr) {cb(); throw OException("Null pointer exception."); }
+
+/**
+ @brief Forwards exception.
+ */
+#define OExceptionForward(code_block) try {code_block} catch (OException& e) {throw e;}
+
+/**
+ @brief Fowards an exception and executes a callback error handler.
+ */
+#define OExceptionForwardCb(cb, code_block) try {code_block} catch (OException& e) {cb(); throw e; }
